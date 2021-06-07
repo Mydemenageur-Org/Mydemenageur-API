243d7eaccd32662ff62afe47d74d53504ae069f0
MrSpoot <61117152+MrSpoot@users.noreply.github.com>
2021-06-07 12:59:38 +0200

    ✅ Add new scenario tests

---
+++ MyDemenageur.ScenarioTests/Helpers/TestApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mydemenageur.ScenarioTests.Helpers
+{
+    public class TestApplicationFactory<TStartup, TTestStartup> : WebApplicationFactory<TTestStartup> where TTestStartup : class
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            var host = Host.CreateDefaultBuilder()
+                            .ConfigureWebHost(builder =>
+                            {
+                                builder.UseStartup<TTestStartup>();
+                            })
+                            .ConfigureAppConfiguration((context, conf) =>
+                            {
+                                var projectDir = Directory.GetCurrentDirectory();
+                                var configPath = Path.Combine(projectDir, "appsettings.json");
+
+                                conf.AddJsonFile(configPath);
+                            });
+
+            return host;
+        }
+    }
+}
---
+++ MyDemenageur.ScenarioTests/Helpers/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Mydemenageur.API;
+using Mydemenageur.ScenarioTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mydemenageur.ScenarioTests.Helpers
+{
+    public abstract class TestBase : IClassFixture<TestApplicationFactory<Startup, FakeStartup>>
+    {
+        protected WebApplicationFactory<FakeStartup> Factory { get; }
+
+        public TestBase(TestApplicationFactory<Startup, FakeStartup> factory)
+        {
+            Factory = factory;
+        }
+    }
+}
