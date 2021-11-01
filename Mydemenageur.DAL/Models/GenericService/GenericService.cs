97a59377c296c7869cbc86cd1dd2ba9d875b0bb6
DENIS Victor <victordenis01@gmail.com>
2021-11-01 12:13:33 +0100

    🚧 Create first model for generic service

---
+++ Mydemenageur.DAL/Models/GenericService/GenericService.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mydemenageur.DAL.Models.GenericService
+{
+    public class GenericService
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public IList<GenericServiceField> Fields { get; set; }
+
+    }
+}
---
+++ Mydemenageur.DAL/Models/GenericService/GenericServiceField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mydemenageur.DAL.Models.GenericService
+{
+    public class GenericServiceField
+    {
+        public string FieldId { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
