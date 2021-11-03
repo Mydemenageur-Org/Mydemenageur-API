79f55622b8b7699543ccace0fdee79f876079040
LeoDELPON <delponleo@gmail.com>
2021-11-03 17:41:35 +0100

    ✨ Modified GenericServiceField model

--- Mydemenageur.DAL/Models/GenericService/GenericServiceField.cs
+++ Mydemenageur.DAL/Models/GenericService/GenericServiceField.cs
@@ -5,10 +5,19 @@ using System.Threading.Tasks;
 
 namespace Mydemenageur.DAL.Models.GenericService
 {
+    public class Attributes
+    {
+        public string Value { get; set; }
+        public string Key { get; set; }
+    }
     public class GenericServiceField
     {
         public string FieldId { get; set; }
         public string Type { get; set; }
         public string Value { get; set; }
+        public string? Placeholder { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public Attributes[]? Attributes { get; set; }
     }
 }
