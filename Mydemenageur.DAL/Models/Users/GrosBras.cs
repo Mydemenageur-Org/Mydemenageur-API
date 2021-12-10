7a602db19e241930a495e7f62d6693adadc9f0c2
LeoDELPON <delponleo@gmail.com>
2021-12-10 09:47:35 +0100

    👽️Add and modify models for migration

---
+++ Mydemenageur.DAL/Models/Users/GrosBras.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using Mydemenageur.DAL.Models.Experiences;
+
+namespace Mydemenageur.DAL.Models.Users
+{
+    public class GrosBras
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string MyDemenageurUserId { get; set; }
+        /// <summary>
+        /// Correspond to the services the user can propose as a potential "Gros Bras"
+        /// </summary>
+        /// <example>1</example>
+        public string[] ServicesProposed { get; set; }
+        /// <summary>
+        /// It displays all diploma or certificates the "Gros Bras" has
+        /// </summary>
+        /// <example>1</example>
+        public Experience[] DiplomaOrExperiences { get; set; }
+        /// <summary>
+        /// Correspond to a description of the "Gros Bras"
+        /// </summary>
+        /// <example>1</example>
+        public string Description { get; set; }
+        /// <summary>
+        /// It is all commitments of the "Gros Bras"
+        /// </summary>
+        /// <example>1</example>
+        public string[] Commitment { get; set; }
+        /// <summary>
+        /// It is the professional status of the "Gros Bras"
+        /// </summary>
+        /// <example>1</example>
+        public string ProStatus { get; set; }
+        public City City { get; set; }
+        public string Departement { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
